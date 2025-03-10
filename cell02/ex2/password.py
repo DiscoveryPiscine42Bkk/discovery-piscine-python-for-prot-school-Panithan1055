3f29dba16a2dbc25791c7dbe2eab820a10c0503b
Panithan1055 <bm8918573@gmail.com>
2025-03-10 16:03:24 +0700

    Add files via upload

---
+++ cell02/ex2/password.py
@@ -0,0 +1,8 @@
+password = "Python is awesome"
+input_password = input()
+
+
+if input_password == password :
+    print("ACCESS GRANTED")
+elif input_password != password :
+    print("ACCESS DENIED")
