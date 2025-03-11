cbd2d64e31f5b099f3f6954dca58c034a77b3af4
Panithan1055 <bm8918573@gmail.com>
2025-03-11 13:14:10 +0700

    Add files via upload

---
+++ cell06/ex02/downcase_all.py
@@ -0,0 +1,16 @@
+def perform(first_number, second_number):
+    print(f"{first_number} + {second_number} = {first_number + second_number}")
+    print(f"{first_number} - {second_number} = {first_number - second_number}")
+    print(f"{first_number} / {second_number} = {first_number / second_number}")
+    print(f"{first_number} * {second_number} = {first_number * second_number}")
+
+def main():
+    first_number = float(input("Give me the first number: "))
+    second_number = float(input("Give me the second number: "))
+    print("Thank you!")
+    perform(first_number, second_number)
+
+
+
+if __name__ == "__main__":
+    main()
